145e0d51abd1d6c5f60eb03c45274aa73f30d65e
study <study@MB-207-03>
2020-01-16 21:28:14 +0800

    java

--- src/main/java/com/web/mvc/entity/one2one/Test2.java
+++ src/main/java/com/web/mvc/entity/one2one/Test2.java
@@ -1,12 +1,21 @@
 package com.web.mvc.entity.one2one;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.web.mvc.entity.JPAUtil;
+import java.util.List;
 import javax.persistence.EntityManager;
 
 public class Test2 {
     static EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
     public static void main(String[] args) {
-        add("Tom", "Jane");
+       // add("John", "Mary");
+//        queryHusband();
+//        queryWife();
+//        get(Husband.class, 1051L);
+//        get(Wife.class, 1201L);
+        
+       // update(1051L, "Vincent", "Anita");
+delete(1151L);
         
     }
     
@@ -23,5 +32,51 @@ public class Test2 {
         em.persist(husband);
         em.getTransaction().commit();
         System.out.println("Add OK !");
+    }
+    public static void queryHusband()throws  Exception{
+	List<Husband> list=em.createQuery("SELECT h FROM Husband h",Husband.class).getResultList();
+   print(list);
+    }
+    
+    public static void queryWife() throws Exception {
+        List<Wife> list = em.createQuery("Select h From Wife h", Wife.class).getResultList();
+        print(list);
+    }
+    
+    public static void get(Class cls, Long id) throws Exception {
+        Object object = em.find(cls, id);
+        print(object);
+    }
+    
+    public static void update(Long id,String name1,String name2) {
+	Husband husband=em.find(Husband.class, id);
+    if(husband==null)return;
+    Wife wife =husband.getWife();
+if(name1!=null){
+husband.setName(name1);
+}
+if(name2!=null){
+wife.setName(name2);
+  em.getTransaction().begin();
+        em.persist(husband);
+        em.getTransaction().commit();
+        System.out.println("update OK !");
+}
+
+    }
+
+    public static void delete(Long id) {
+        Husband husband = em.find(Husband.class, id);
+        if(husband == null) return;
+        em.getTransaction().begin();
+        em.remove(husband);
+        em.getTransaction().commit();
+        System.out.println("Delete OK !");
+    }
+    
+    public static void print(Object object) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(object);
+        System.out.println(json);
     }
 }
